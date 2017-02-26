fe0de5a2967fe718d1d078ccfbce44a05b89b686
Sergey <silver2006@tut.by>
2017-02-26 17:13:57 +0300

    Create class MenuView

--- src/io/hexlett/java/m101/xo/Main.java
+++ src/io/hexlett/java/m101/xo/Main.java
@@ -1,16 +1,15 @@
 package io.hexlett.java.m101.xo;
 
-import io.hexlett.java.m101.xo.controller.GameController;
-import io.hexlett.java.m101.xo.model.Board;
-import io.hexlett.java.m101.xo.model.Figure;
-import io.hexlett.java.m101.xo.model.Player;
-import io.hexlett.java.m101.xo.view.ConsoleView;
+import io.hexlett.java.m101.xo.view.MenuView;
 
 public class Main {
     public static void main(String[] args) {
 
-        final Board board = new Board();
-        final GameController gameController = new GameController("XO", new Player[]{new Player("Slava", new Figure("O")), new Player("Gleb", new Figure("X"))}, board);
-        new ConsoleView(gameController).showPlayers();
+//        final Board board = new Board();
+//        final GameController gameController = new GameController("XO", new Player[]{new Player("Slava", new Figure("O")), new Player("Gleb", new Figure("X"))}, board);
+//        new ConsoleView(gameController).showPlayers();
+
+        MenuView menu = new MenuView();
+        menu.showMenuWithResult();
     }
 }
