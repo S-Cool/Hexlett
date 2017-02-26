b16e7977076f31aedc2dd8e6d00bcb3b89455560
Sergey <silver2006@tut.by>
2017-02-26 17:15:30 +0300

    Plus lose file

---
+++ src/io/hexlett/java/m101/xo/view/MenuView.java
@@ -0,0 +1,38 @@
+package io.hexlett.java.m101.xo.view;
+
+import java.util.Scanner;
+
+public class MenuView {
+    private static final int START_CODE = 1;
+
+    public int showMenuWithResult() {
+        System.out.println(START_CODE + " - Start");
+        System.out.println("2 - Load");
+        System.out.println("3 - Settings");
+        System.out.println("4 - Exit");
+        System.out.println("> ");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("START!");
+                break;
+            case 2:
+                System.out.println("LOAD!");
+                break;
+            case 3:
+                System.out.println("SETTINGS!");
+                break;
+            case 4:
+                System.out.println("EXIT!");
+                break;
+            default:
+                System.out.println("REALLY!?");
+                break;
+
+        }
+        return 0;
+    }
+}
