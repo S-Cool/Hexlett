62e7bea10ea9670b5efe70017b156302f1adf117
Sergey <silver2006@tut.by>
2017-03-05 19:20:06 +0300

    Learn pattern Builder

--- src/io/hexlett/java/m101/xo/Main.java
+++ src/io/hexlett/java/m101/xo/Main.java
@@ -1,6 +1,8 @@
 package io.hexlett.java.m101.xo;
 
 import io.hexlett.java.m101.xo.controller.GameController;
+import io.hexlett.java.m101.xo.model.Contact;
+import io.hexlett.java.m101.xo.model.ContactBuilder;
 import io.hexlett.java.m101.xo.view.AdvConsoleView;
 import io.hexlett.java.m101.xo.view.ConsoleView;
 import io.hexlett.java.m101.xo.view.MenuView;
@@ -12,9 +14,17 @@ public class Main {
 //        final Board board = new Board();
 //        final GameController gameController = new GameController("XO", new Player[]{new Player("Slava", new Figure("O")), new Player("Gleb", new Figure("X"))}, board);
 //        new ConsoleView(gameController).showPlayers();
+//
+//        MenuView menu = new MenuView();
+//        menu.showMenuWithResult();
 
-        MenuView menu = new MenuView();
-        menu.showMenuWithResult();
+        final Contact contact = new ContactBuilder()
+                .name("Slava")
+                .surname("Koval")
+                .mail("test@nmail.ru")
+                .phone("5555555")
+                .address("street")
+                .build();
     }
 //
 //public static void testConsoleView(){
