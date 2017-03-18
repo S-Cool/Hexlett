a13732f6effbad3b2473035b9d721e3feb6efd00
Sergey <silver2006@tut.by>
2017-03-18 19:14:18 +0300

    Refactoring Field

--- src/io/hexlett/java/m101/xo/model/Field.java
+++ src/io/hexlett/java/m101/xo/model/Field.java
@@ -1,20 +1,23 @@
 package io.hexlett.java.m101.xo.model;
 
-import io.hexlett.java.m101.xo.exceptions.AlreadyOccupiedException;
 import io.hexlett.java.m101.xo.exceptions.InvalidePointException;
 
 import java.awt.*;
 
 public class Field {
 
-    private static final int SIZE_FIELD = 3;
-    private static final int MIN_COORDINATE = 0;
-    private static final int MAX_COORDINATE = SIZE_FIELD;
+    private int fieldSize = 3;
 
-    private Figure[][] field = new Figure[SIZE_FIELD][SIZE_FIELD];
+    private static final int MIN_COORDINATE = 0;
+    private Figure[][] field;
+
+    public Field(final int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize() {
-        return SIZE_FIELD;
+        return fieldSize;
     }
 
     public Figure getFigure(final Point point)throws InvalidePointException{
@@ -32,10 +35,10 @@ public class Field {
     }
 
     private boolean checkPoint(final Point point) {
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
     }
 
-    private boolean checkCoordinate(final int coordinate) {
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < maxCoordinate;
     }
 }
