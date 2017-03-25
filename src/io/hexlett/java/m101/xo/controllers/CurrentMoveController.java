b51a24783c1a203df6951ff5b5c8761cf4f98ba1
Sergey <silver2006@tut.by>
2017-03-25 13:51:36 +0300

    Creating CurrentMoveController

---
+++ src/io/hexlett/java/m101/xo/controllers/CurrentMoveController.java
@@ -0,0 +1,33 @@
+package io.hexlett.java.m101.xo.controllers;
+
+import io.hexlett.java.m101.xo.exceptions.InvalidPointException;
+import io.hexlett.java.m101.xo.model.Field;
+import io.hexlett.java.m101.xo.model.Figure;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Field field) {
+        int countFigure = 0;
+        for (int x = 0; x < field.getSize(); x++) {
+           countFigure += countFigureInTheRow(field, x);
+        }
+        if (countFigure % 2 == 0)
+            return Figure.X;
+
+        return Figure.O;
+    }
+    private int countFigureInTheRow (final Field field, final int row){
+        int countFigure = 0;
+        for (int x = 0; x < field.getSize(); x++) {
+            try {
+                if (field.getFigure(new Point(x, row)) != null)
+                    countFigure++;
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            }
+        }
+        return countFigure;
+    }
+}
