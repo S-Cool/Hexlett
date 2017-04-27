a0dff3ad30396138dc4f218e86b8069961941f02
Sergey <silver2006@tut.by>
2017-04-27 21:57:45 +0300

    Create Example with Merge Maps

---
+++ src/io/kuleshov/map/MapExample4Merge.java
@@ -0,0 +1,29 @@
+package io.kuleshov.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class MapExample4Merge {
+    public static void main(String[] args) {
+        final Map<String, Student> students = new HashMap<String, Student>() {{
+            put("12", new Student("12", "Slava"));
+            put("11", new Student("13", "Gleb"));
+        }};
+        final Map<String, Student> students2 = new HashMap<String, Student>() {{
+            put("12", new Student("13", "Slava"));
+            put("11", new Student("12", "Gleb"));
+            put("13", new Student("13", "Oleh"));
+        }};
+        for (Map.Entry<String, Student> e : students2.entrySet())
+            students.merge(e.getKey(), e.getValue(), new BiFunction<Student, Student, Student>() {
+
+                @Override
+                public Student apply(Student student, Student student2) {
+                    if (student.getId().equals(e.getKey())) return student;
+                    return student2;
+                }
+            });
+        System.out.println(students);
+    }
+}
