71e86a527fba11c2bafe39511de1932d57d26b0b
Sergey <silver2006@tut.by>
2017-06-14 22:30:19 +0300

    Learn pattern builder

---
+++ src/io/kuleshov/builder/Contact.java
@@ -0,0 +1,33 @@
+package io.kuleshov.builder;
+
+public class Contact {
+    private final String name;
+
+    private final String surName;
+
+    private final String mail;
+
+    private final String phone;
+
+    private final String address;
+
+     Contact(final ContactBuilder contactBuilder) {
+        this.name = contactBuilder.getName();
+        this.surName = contactBuilder.getSurName();
+        this.mail = contactBuilder.getMail();
+        this.phone = contactBuilder.getPhone();
+        this.address = contactBuilder.getAddress();
+    }
+
+    public static void main(String[] args) {
+final  Contact contact = new ContactBuilder()
+        .name("Slave")
+        .surName("sur")
+        .address("add")
+        .phone("555-55-55")
+        .mail("mail@mail.com")
+        .build();
+    }
+}
+
+
---
+++ src/io/kuleshov/builder/ContactBuilder.java
@@ -0,0 +1,58 @@
+package io.kuleshov.builder;
+
+public class ContactBuilder {
+    private String name;
+    private String surName;
+    private String mail;
+    private String phone;
+    private String address;
+
+    public ContactBuilder name(final String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ContactBuilder surName(final String surName) {
+        this.surName = surName;
+        return this;
+    }
+
+    public ContactBuilder mail(final String mail) {
+        this.mail = mail;
+        return this;
+    }
+
+    public ContactBuilder phone(final String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public ContactBuilder address(final String address) {
+        this.address = address;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Contact build() {
+        return new Contact(this);
+    }
+}
