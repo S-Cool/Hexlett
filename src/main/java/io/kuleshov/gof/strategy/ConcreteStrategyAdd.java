ea9da37639c2b8700db1216c0e157d5c5b449e51
Sergey <silver2006@tut.by>
2017-07-04 22:21:47 +0300

    Implementation pattern strategy

---
+++ src/main/java/io/kuleshov/gof/strategy/ConcreteStrategyAdd.java
@@ -0,0 +1,10 @@
+package io.kuleshov.gof.strategy;
+
+// Реализуем алгоритм с использованием интерфейса стратегии
+class ConcreteStrategyAdd implements Strategy {
+
+    public int execute(int a, int b) {
+        System.out.println("Called ConcreteStrategyAdd's execute()");
+        return a + b;  // Do an addition with a and b
+    }
+}
