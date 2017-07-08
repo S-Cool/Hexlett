08c5e873c1e6a6ffa87fcf6bf91f109b80f26496
Sergey <silver2006@tut.by>
2017-07-08 09:27:37 +0300

    Implementation Singleton with Double-checked locking

--- src/main/java/io/kuleshov/gof/singleton/SingletonImpl.java
+++ src/main/java/io/kuleshov/gof/singleton/SingletonImpl.java
@@ -10,19 +10,19 @@ public class SingletonImpl{
         self = this;
     }
 
-    public static synchronized SingletonImpl getInstance(){
-        return (self == null) ? new SingletonImpl() : self;
-    }
-
-//    public static SingletonImpl getInstance(){
-//        if (self == null){
-//            synchronized(SingletonImpl.class){
-//                if (self == null){
-//                    self = new SingletonImpl();
-//                }
-//            }
-//        }
-//        return self;
+//    public static synchronized SingletonImpl getInstance(){
+//        return (self == null) ? new SingletonImpl() : self;
 //    }
+
+    public static SingletonImpl getInstance(){
+        if (self == null){
+            synchronized(SingletonImpl.class){
+                if (self == null){
+                    self = new SingletonImpl();
+                }
+            }
+        }
+        return self;
+    }
 
 }
