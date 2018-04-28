3257dc80bd0e0862056d96021562b272bb4bf35f
Sergey <silver2006@tut.by>
2018-04-28 22:11:14 +0300

    chore(HackerRank) Java Anagrams

--- src/main/java/io/kuleshov/hackerrank/Solution.java
+++ src/main/java/io/kuleshov/hackerrank/Solution.java
@@ -1,25 +1,69 @@
 package io.kuleshov.hackerrank;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    static boolean isAnagram(String a, String b) {
 
-        Scanner sc = new Scanner(System.in);
-        String word = sc.next();
-        char[] charArray = word.toCharArray();
-        String answer = "Yes";
-        int begin = 0;
-        int finish = word.length() - 1;
+// test for invalid input
+        if (a == null || b == null || a.equals("") || b.equals(""))
+            throw new IllegalArgumentException();
 
-        while (finish > begin) {
-            if (charArray[begin] != charArray[finish]) {
-                answer = "No";
+// initial quick test for non-anagrams
+        if (a.length() != b.length())
+            return false;
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+// populate a map with letters and frequencies of String b
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int k = 0; k < b.length(); k++) {
+            char letter = b.charAt(k);
+
+            if (!map.containsKey(letter)) {
+                map.put(letter, 1);
+            } else {
+                Integer frequency = map.get(letter);
+                map.put(letter, ++frequency);
             }
-            ++begin;
-            --finish;
         }
-        System.out.println(answer);
+
+// test each letter in String a against data in the map
+// return if letter is absent in the map or its  frequency is 0
+// otherwise decrease the frequency by 1
+
+        for (int k = 0; k < a.length(); k++) {
+            char letter = a.charAt(k);
+
+            if (!map.containsKey(letter))
+                return false;
+
+            Integer frequency = map.get(letter);
+
+            if (frequency == 0)
+                return false;
+            else
+                map.put(letter, --frequency);
+        }
+// if the code got that far it is an anagram
+        return true;
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
     }
 }
