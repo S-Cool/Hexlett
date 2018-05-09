ddf124956674d0e98ec1c7848f9ceba2bcc54905
Sergey <silver2006@tut.by>
2018-05-09 21:40:57 +0300

    chore(HackerRank) Mini-Max Sum

--- src/main/java/io/kuleshov/hackerrank/Solution.java
+++ src/main/java/io/kuleshov/hackerrank/Solution.java
@@ -1,15 +1,41 @@
 package io.kuleshov.hackerrank;
 
-import java.util.Scanner;
+import java.util.*;
 
-class Solution {
+public class Solution {
+
+    static void miniMaxSum(int[] arr) {
+
+        long sum = 0;
+        long min = 1000000000;
+        long max = 0;
+
+        for (int i = 0; i < 5; i++) {
+
+            sum = sum + arr[i];
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println((sum - max) + " " + (sum - min));
+    }
+
+
+    private static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        while (in.hasNext()) {
-            String IP = in.next();
-            System.out.println(IP.matches(new MyRegex().pattern));
+        int[] arr = new int[5];
+
+        String[] arrItems = scan.nextLine().split(" ");
+
+        for (int arrItr = 0; arrItr < 5; arrItr++) {
+            int arrItem = Integer.parseInt(arrItems[arrItr].trim());
+            arr[arrItr] = arrItem;
         }
 
+        miniMaxSum(arr);
     }
 }
